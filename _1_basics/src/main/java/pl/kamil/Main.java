6c2ac1e86bdeae1156b271dc421d756eb5ed4348
Kamil Figoń <kamil.figon@jitsolutions.pl>
2018-12-18 21:52:08 +0100

    logging enabled

--- _1_basics/src/main/java/pl/kamil/Main.java
+++ _1_basics/src/main/java/pl/kamil/Main.java
@@ -6,16 +6,12 @@ import org.apache.camel.impl.DefaultCamelContext;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("yo! Starting route...");
-
         CamelContext camelContext = new DefaultCamelContext();
         camelContext.addRoutes(new MyRoute());
 
-        System.out.println("start route...");
         camelContext.start();
         Thread.sleep(2000);
 
-        System.out.println("stop route");
         camelContext.stop();
     }
 }
